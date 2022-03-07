9ffefbb8a536dfd68ff7a789d4b48674d2e73f14
Robert Tiempo <jroberttiempo58@gmail.com>
2022-03-07 16:35:24 +0800

    Fix overflow on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,35 +1,19 @@
 import React, { useState } from 'react';
-import {
-  Nav,
-  Logo,
-  Burger,
-  Menu,
-  MenuLink,
-  SearchBar,
-  Input,
-  Icon,
-} from './Navbar.styles';
+import { Appbar, Title, SearchBar, Input, Icon } from './Navbar.styles';
 import { FaSearch } from 'react-icons/fa';
 import { Autocomplete } from '@react-google-maps/api';
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
   return (
-    <Nav>
-      <Logo>where to eat</Logo>
-      <Burger></Burger>
+    <Appbar>
+      <Title>where to eat</Title>
       <SearchBar>
         <Input type='text' placeholder='Search...' />
         <Icon>
           <FaSearch />
         </Icon>
       </SearchBar>
-      <Menu isOpen={isOpen}>
-        <MenuLink to='About' smooth={true} duration={1000}>
-          Test
-        </MenuLink>
-      </Menu>
-    </Nav>
+    </Appbar>
   );
 };
 
--- src/components/Navbar/Navbar.styles.js
+++ src/components/Navbar/Navbar.styles.js
@@ -1,80 +1,46 @@
 import styled from 'styled-components';
 
-export const Nav = styled.nav`
+export const Appbar = styled.nav`
   position: fixed;
   top: 0;
-  width: 100vw;
-  height: 8vh;
+  width: 100%;
+  height: 9vh;
+  padding: 0 2em;
+  display: flex;
   justify-content: space-between;
   align-items: center;
-  flex-wrap: wrap;
-  display: flex;
-  background: #275553;
-  box-shadow: ${({ isTop }) =>
+  box-sizing: border-box;
+  background-color: #275553;
+  /* box-shadow: ${({ isTop }) =>
     isTop ? '0' : '0 4px 16px rgba(0, 0, 0, 0.07)'};
   z-index: 1;
   opacity: ${({ scrollDown }) => (scrollDown ? '0' : '1')};
-  transition: all 0.3s ease-out;
+  transition: all 0.3s ease-out; */
 
   @media screen and (max-width: 768px) {
     padding: 0 1em;
   }
 `;
 
-export const Menu = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: relative;
-
-  @media screen and (max-width: 768px) {
-    overflow: hidden;
-    flex-direction: column;
-    max-height: ${({ isOpen }) => (isOpen ? '300px' : '0')};
-    transition: max-height 0.3s ease-in;
-    width: 100%;
-  }
-`;
-
-export const MenuLink = styled.a`
-  margin: 0.5em 1em;
-  cursor: pointer;
-  text-align: center;
-  text-decoration: none;
+export const Title = styled.h3`
   color: #fff;
-  transition: all 0.3s ease-out;
-
-  &:hover {
-    color: #14919b;
-    border-bottom: 2px solid #14919b;
-  }
-`;
-
-export const Burger = styled.div`
-  display: none;
-
-  @media screen and (max-width: 768px) {
-    display: flex;
-  }
-`;
-
-export const Logo = styled.div`
-  color: #fff;
-  text-decoration: none;
 `;
 
 export const SearchBar = styled.div`
-  background-color: #fff;
+  background-color: #eef7f6;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0.5em;
-  border-radius: 10px;
+  border-radius: 0.5em;
+
+  &:hover {
+    background-color: #fff;
+  }
 `;
 
 export const Input = styled.input`
-  flex: 1 0;
-  min-width: 200px;
+  min-width: 25vw;
   min-height: 25px;
   background-color: inherit;
   padding-left: 1em;
