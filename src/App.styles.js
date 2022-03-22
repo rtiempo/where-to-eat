ed0b9426b6dff357d66885a3a077224652c86cb1
Robert Tiempo <jroberttiempo58@gmail.com>
2022-03-22 21:42:55 +0800

    Add initial map styles

--- src/App.styles.js
+++ src/App.styles.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 export const Grid = styled.div`
   display: grid;
   gap: 1.5rem;
-  grid-auto-columns: 1fr;
-  grid-template-rows: 9vh 91vh;
+  grid-template-columns: 1fr 1.5fr 1fr;
+  grid-template-rows: 10% 90%;
   grid-template-areas:
     'nav nav nav'
     'details map list';
